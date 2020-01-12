9c0491ae93edb1b62579a1d26e9873fbfff81517
Kevin O'Toole <kotoole@onshape.com>
2020-01-12 12:05:13 -0500

    default unit url

--- src/models/unitModel.ts
+++ src/models/unitModel.ts
@@ -15,7 +15,7 @@ export class UnitModel {
 
   public unitSizeId: string = '_1D6';
 
-  public imageUrl: string = 'light-axemen.jpg';
+  public imageUrl: string = 'https://i.pinimg.com/474x/e7/c6/05/e7c605dbff10da18b55694dbe1748596--the-wake-anglo-saxon.jpg';
   public imageScale: number = 120;
   public imageXPos: number = 50;
   public imageYPos: number = 50;
